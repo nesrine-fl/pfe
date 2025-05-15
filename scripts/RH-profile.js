67ff66f81bdc7edb39478b296360b7fe68c2f2e3
nesrine-fl <nesrinefettal26@gmail.com>
2025-05-15 10:50:19 +0100

    Update RH-profile.js

--- scripts/RH-profile.js
+++ scripts/RH-profile.js
@@ -1,12 +1,3 @@
-function toggleSidebar() {
-    var sidebar = document.getElementById("sidebar");
-    // Check the current width of the sidebar and adjust it
-    if (sidebar.style.width === "250px") {
-        sidebar.style.width = "0"; // Close the sidebar
-    } else {
-        sidebar.style.width = "250px"; // Open the sidebar
-    }
-}
 
 
 
@@ -295,3 +286,13 @@ document.addEventListener('DOMContentLoaded', function() {
     // Vérifie périodiquement les nouvelles demandes
     setInterval(checkFormationRequests, 1000);
 }); 
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    // Check the current width of the sidebar and adjust it
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0"; // Close the sidebar
+    } else {
+        sidebar.style.width = "250px"; // Open the sidebar
+    }
+}
+
